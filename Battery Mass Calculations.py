97b2dc989835620c0d1372b70fb61f37749bacb0
Theo <t.s.radomirescu@student.tudelft.nl>
2025-05-12 12:26:16 +0200

    Update Battery Mass Calculations.py

--- Battery Mass Calculations.py
+++ Battery Mass Calculations.py
@@ -1,2 +1,11 @@
 # Battery Mass Calculations
 
+t = 0           # Duration of Mission Segment 
+P = 0           # Power Required
+M_to = 0        # Take-off Mass
+E_spec = 0      # Specific Energy Capacity
+Eta_bat = 0     # Battery Efficiency
+f_usable = 0    # Usable Battery Capacity
+
+
+M_frac_battery = (t * P) / ( M_to * E_spec * Eta_bat * f_usable)
