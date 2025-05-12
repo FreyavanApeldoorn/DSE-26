bf750f0ff6036aa913e83a6b6d6254069cfe8e43
Theo <t.s.radomirescu@student.tudelft.nl>
2025-05-12 13:34:54 +0200

    Update Battery Mass Calculations.py

--- Battery Mass Calculations.py
+++ Battery Mass Calculations.py
@@ -8,8 +8,14 @@ E_spec = 0      # Specific Energy Capacity
 Eta_bat = 0     # Battery Efficiency
 f_usable = 0    # Usable Battery Capacity
 g = 9.81        # Gravitational Acceleration
+Eta_electric = 0    # Efficiency of electric system
+T = 0           # Thrust
+DL = 0          # Disc Loading
+rho = 0         # Air Density
 
+
+FM = 0.4742*T**(0.0793)          # Rotor Efficiency
 
 M_frac_battery = (t * P) / ( M_to * E_spec * Eta_bat * f_usable)
 
-M_frac_battery_hover = (t_hover * g) / ( M_to * E_spec * Eta_bat * f_usable)
+M_frac_battery_hover = ((t_hover * g) / ( E_spec * FM * Eta_electric * Eta_bat * f_usable))*(DL/(2*rho))**0.5
