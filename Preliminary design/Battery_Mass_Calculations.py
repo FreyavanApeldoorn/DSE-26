dd2e94fe58ca9bd341748f48a5bcb30adec634da
pragyaagrawal117 <160593813+pragyaagrawal117@users.noreply.github.com>
2025-05-13 11:30:26 +0200

    update for battery mass

--- Preliminary design/Battery_Mass_Calculations.py
+++ Preliminary design/Battery_Mass_Calculations.py
@@ -5,15 +5,12 @@ rho = 0.9013      # Air Density [kg/m^3]
 R = 60000         # Range [m]
 R_C = 3           # rate of climb [m/s]
 
-
-
-
-
 class Batt_Mass:
 
     def __init__(self, t_hover: float, t_loiter:float, M_to:float, E_spec:float, Eta_bat: float,
-                 f_usable:float, Eta_electric:float, T:float, DL:float, LD_max:float, CL:float, 
-                   CD:float, WS:float, h_end:float, h_start:float, P_Prop:float  ):         
+                f_usable:float, Eta_electric:float, T:float, DL:float, LD_max:float, CL:float, 
+                CD:float, WS:float, h_end:float, h_start:float, P_Prop:float  ):       
+          
         self.t_hover = t_hover             # Duration of hover [s]
         self.t_loiter = t_loiter           # Duration of loiter (horizontal flying) [s]
         self.M_to = M_to                   # Take-off Mass [kg]
