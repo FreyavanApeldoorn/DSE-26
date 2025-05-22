3d9a66340d81e74a27b5311ed7f2acff77c22bf2
jpavilap <126147766+jpavilap@users.noreply.github.com>
2025-05-22 08:24:29 +0200

    a

--- PreliminaryDesign/integration.py
+++ PreliminaryDesign/integration.py
@@ -5,50 +5,49 @@ from Battery_mass_estimation_v2 import calculate_battery_mass
 import numpy as np
 
 
-
 inputs = {
-    #"rho_min": 0.9013,              # kg/m^3, density of air at service ceiling
-    #"MTOW": 30 * 9.81,          # N, maximum take-off weight 
-    #"V_cruise": 100 / 3.6,      # m/s, cruise speed
-    #"V_stall": 13.8,             # m/s, stall speed
-    #"CD0": 0.040,               # drag coefficient at zero lift
-    #"n_p": 0.85,                # propeller efficiency
-    #"R_C_service": 0.5,         # service ceiling ratio
-    #"CL_max": 1.34,             # maximum lift coefficient
-    #"e": 0.7,                   # Oswald's efficiency factor
-    #"AR": 10.3,                 # aspect ratio
-    #"stot_s_w": 1.35,           # wing loading ratio
-    #"eta_prop": 0.83,           # propeller efficiency
-    #"U_max": 25.5,              # maximum voltage
+    # "rho_min": 0.9013,              # kg/m^3, density of air at service ceiling
+    # "MTOW": 30 * 9.81,          # N, maximum take-off weight
+    # "V_cruise": 100 / 3.6,      # m/s, cruise speed
+    # "V_stall": 13.8,             # m/s, stall speed
+    # "CD0": 0.040,               # drag coefficient at zero lift
+    # "n_p": 0.85,                # propeller efficiency
+    # "R_C_service": 0.5,         # service ceiling ratio
+    # "CL_max": 1.34,             # maximum lift coefficient
+    # "e": 0.7,                   # Oswald's efficiency factor
+    # "AR": 10.3,                 # aspect ratio
+    # "stot_s_w": 1.35,           # wing loading ratio
+    # "eta_prop": 0.83,           # propeller efficiency
+    # "U_max": 25.5,              # maximum voltage
     # "F1": 0.889,                # motor constant (kg/W^E1 * V^E2)
     # "E1": -0.288,               # exponent for power
     # "E2": 0.1588,               # exponent for voltage
     # "f_install_cruise": 1,      # cruise propulsion installation factor
     # "f_install_vtol": 1,        # VTOL propulsion installation factor
-    #"n_mot_cruise": 1,          # number of cruise motors
-    #"n_mot_vtol": 4,            # number of VTOL motors
-    #"K_material": 0.6,          # material constant (kg/m^3)
-    #"n_propellers_cruise": 1,        # number of cruise propellers
-    #"n_propellers_vtol": 4,          # number of VTOL propellers
-    #"n_blades_cruise": 4,       # number of blades per cruise propeller
-    #"n_blades_vtol": 4,         # number of blades per VTOL propeller
-    #"K_p": 0.0938,              # propeller constant (kg/W^E1 * V^E2)
+    # "n_mot_cruise": 1,          # number of cruise motors
+    # "n_mot_vtol": 4,            # number of VTOL motors
+    # "K_material": 0.6,          # material constant (kg/m^3)
+    # "n_propellers_cruise": 1,        # number of cruise propellers
+    # "n_propellers_vtol": 4,          # number of VTOL propellers
+    # "n_blades_cruise": 4,       # number of blades per cruise propeller
+    # "n_blades_vtol": 4,         # number of blades per VTOL propeller
+    # "K_p": 0.0938,              # propeller constant (kg/W^E1 * V^E2)
     # "t_hover": 4 * 60,          # hover time in seconds
     # "t_loiter": 0,              # loiter time in seconds
-    #"E_spec": 168,              # specific energy of the battery (Wh/kg)
-    #"Eta_bat": 0.95,            # battery efficiency
+    # "E_spec": 168,              # specific energy of the battery (Wh/kg)
+    # "Eta_bat": 0.95,            # battery efficiency
     # "f_usable": 6000,           # usable battery capacity (Wh)
     # "Eta_electric": 0.95,       # electric system efficiency
     # "LD_max": 12,               # maximum lift-to-drag ratio
-    #"CL_cruise": 0.846,                # lift coefficient
-    #"CD_cruise": 0.04,                 # drag coefficient
-    #"T": 30 * 9.81,             # thrust in N
-    #"h_end": 100,               # end altitude in m
-    #"h_start": 0,               # start altitude in m
+    # "CL_cruise": 0.846,                # lift coefficient
+    # "CD_cruise": 0.04,                 # drag coefficient
+    # "T": 30 * 9.81,             # thrust in N
+    # "h_end": 100,               # end altitude in m
+    # "h_start": 0,               # start altitude in m
     # "MF_struct": 0.35,          # mass fraction for structure
     # "MF_avion": 0.05,           # mass fraction for avionics
     # "MF_Subsyst": 0.07,         # mass fraction for subsystems
-    #"M_payload": 5,             # payload mass in kg
+    # "M_payload": 5,             # payload mass in kg
 }
 
 
@@ -56,202 +55,209 @@ inputs = {
 
 
 constants = {
-    "g": 9.81,                  # gravitational acceleration in m/s^2
+    "g": 9.81,  # gravitational acceleration in m/s^2
 }
 
-mission_definition = []   # Define the mission profile here
+mission_definition = []  # Define the mission profile here
 
-# Mission profile parameters: 
+# Mission profile parameters:
 mission_parameters = {
-    "h_ground": 0,     # ground altitude in m
-    "h_cruise": 120,    # cruise altitude in m
-    "rho_min": 0.9013, 
-    "rho_max": 1.225, 
+    "h_ground": 0,  # ground altitude in m
+    "h_cruise": 120,  # cruise altitude in m
+    "rho_min": 0.9013,
+    "rho_max": 1.225,
     "RC_service": 0.5,
     "h_service": 3000,
-    "R_max": 30000, #30000
-    "V_climb_v": 6, 
-    "V_cruise": 120/3.6, 
-    "V_descent": 3
-    }   
+    "R_max": 40000,  # 30000
+    "V_climb_v": 6,
+    "V_cruise": 120 / 3.6,
+    "V_descent": 3,
+}
 inputs.update(mission_parameters)
 
 # Mission time estimates
 time_estimates = {
-    "t_load": 1*60, 
-    "t_transition": 30, 
-    "t_scan": 60, 
-    "t_deploy": 5*60, 
-    "t_recharge": 5*60,
-    "t_hover": 4 * 60,          # hover time in seconds
-    "t_loiter": 0,              # loiter time in seconds
-    }
+    "t_load": 1 * 60,
+    "t_transition": 30,
+    "t_scan": 60,
+    "t_deploy": 5 * 60,
+    "t_recharge": 5 * 60,
+    "t_hover": 4 * 60,  # hover time in seconds
+    "t_loiter": 0,  # loiter time in seconds
+}
 inputs.update(time_estimates)
 
-# Power parameters: 
+# Power parameters:
 power_parameters = {
-    "P_load": 100, 
-    "P_r_VTOL": 3500, 
-    "P_r_FW": 1100, 
-    "P_a_transition": 4600, 
-    "P_r": 3500, 
+    "P_load": 100,
+    "P_r_VTOL": 3500,
+    "P_r_FW": 1100,
+    "P_a_transition": 4600,
+    "P_r": 3500,
     "P_deploy": 4000,
-
-    "E_spec": 168,              # specific energy of the battery (Wh/kg)
-    "Eta_bat": 0.95,            # battery efficiency
-    "f_usable": 6000,           # usable battery capacity (Wh)
-    "Eta_electric": 0.95,       # electric system efficiency
-    "U_max": 25.5              # maximum voltage
-    
-    }
+    "E_spec": 168,  # specific energy of the battery (Wh/kg)
+    "Eta_bat": 0.95,  # battery efficiency
+    "f_usable": 6000,  # usable battery capacity (Wh)
+    "Eta_electric": 0.95,  # electric system efficiency
+    "U_max": 25.5,  # maximum voltage
+}
 inputs.update(power_parameters)
 
 
 # Fixed-wing parameters:
 fw_parameters = {
-    "CL_cruise": 0.846, 
-    "CL_max": 1.34, 
-    "LD_max": 12,               # maximum lift-to-drag ratio
-    "t_w": 0,                   # Thrust-to-weight ratio (initialized to 0)
-    "CD_cruise": 0.05, 
-    "CD0": 0.040, 
-    "V_stall": 19,             # m/s, stall speed
-    "S_wing": 2.5, 
-    "AR": 7, #10.3
-    "b_wing": 0,                # span (initialized to 0)
+    "CL_cruise": 0.846,
+    "CL_max": 1.34,
+    "LD_max": 12,  # maximum lift-to-drag ratio
+    "t_w": 0,  # Thrust-to-weight ratio (initialized to 0)
+    "CD_cruise": 0.05,
+    "CD0": 0.040,
+    "V_stall": 19,  # m/s, stall speed
+    "S_wing": 2.5,
+    "AR": 7,  # 10.3
+    "b_wing": 0,  # span (initialized to 0)
     "e": 0.7,
-    "n_propellers_cruise": 1,        # number of cruise propellers
-    "n_blades_cruise": 4,       # number of blades per cruise propeller
+    "n_propellers_cruise": 1,  # number of cruise propellers
+    "n_blades_cruise": 4,  # number of blades per cruise propeller
     "propeller_efficiency_cruise": 0.8,
-    "n_mot_cruise": 1          # number of cruise motors
-    }
+    "n_mot_cruise": 1,  # number of cruise motors
+}
 inputs.update(fw_parameters)
 
 
 # VTOL parameters:
 vtol_parameters = {
-    "T_VTOL": 30 * 9.81,             # thrust in N
-    "number_propellers": 4, 
-    "number_motors": 4, 
-    "propeller_diameter": 0.5, 
-    "propeller_pitch": 0.2, 
-    "n_propellers_vtol": 4,          # number of VTOL propellers
-    "n_blades_vtol": 4,         # number of blades per VTOL propeller
-    "propeller_efficiency_vtol": 0.8,   # VTOL propeller efficiency
-    "n_mot_vtol": 4            # number of VTOL motors
-    }
+    "T_VTOL": 30 * 9.81,  # thrust in N
+    "number_propellers": 4,
+    "number_motors": 4,
+    "propeller_diameter": 0.5,
+    "propeller_pitch": 0.2,
+    "n_propellers_vtol": 4,  # number of VTOL propellers
+    "n_blades_vtol": 4,  # number of blades per VTOL propeller
+    "propeller_efficiency_vtol": 0.8,  # VTOL propeller efficiency
+    "n_mot_vtol": 4,  # number of VTOL motors
+}
 inputs.update(vtol_parameters)
 
 
 # Mass parameters:
 mass_parameters = {
     "MTOW": 30 * 9.81,
-    "M_to": 30,             # take off mass in kg
-    "M_payload": 5, 
-    "M_battery": 0,        # initiialise to 0, will be calculated later
-    #"M_FW": 5, 
-    #"M_VTOL": 5,
-    "MF_struct": 0.35,          # mass fraction for structure
-    "MF_avion": 0.05,           # mass fraction for avionics
-    "MF_Subsyst": 0.07         # mass fraction for subsystems
-    }
+    "M_to": 30,  # take off mass in kg
+    "M_payload": 5,
+    "M_battery": 0,  # initiialise to 0, will be calculated later
+    # "M_FW": 5,
+    # "M_VTOL": 5,
+    "MF_struct": 0.35,  # mass fraction for structure
+    "MF_avion": 0.05,  # mass fraction for avionics
+    "MF_Subsyst": 0.07,  # mass fraction for subsystems
+}
 inputs.update(mass_parameters)
 
 
 # Structural parameters:
 structural_parameters = {
-    "K_material": 0.6,          # material constant (kg/m^3)
+    "K_material": 0.6,  # material constant (kg/m^3)
 }
 inputs.update(structural_parameters)
 
 
 mass_estimation_parameters = {
-    "stot_s_w": 1.35,       # wing loading ratio
-    "F1": 0.889,                # motor constant (kg/W^E1 * V^E2)
-    "E1": -0.288,               # exponent for power
-    "E2": 0.1588,               # exponent for voltage
-    "K_p": 0.0938,              # propeller constant (kg/W^E1 * V^E2)
-    "f_install_cruise": 1,      # cruise propulsion installation factor
-    "f_install_vtol": 1,        # VTOL propulsion installation factor
-    }
+    "stot_s_w": 1.35,  # wing loading ratio
+    "F1": 0.889,  # motor constant (kg/W^E1 * V^E2)
+    "E1": -0.288,  # exponent for power
+    "E2": 0.1588,  # exponent for voltage
+    "K_p": 0.0938,  # propeller constant (kg/W^E1 * V^E2)
+    "f_install_cruise": 1,  # cruise propulsion installation factor
+    "f_install_vtol": 1,  # VTOL propulsion installation factor
+}
 inputs.update(mass_estimation_parameters)
 
 battery_parameters = {
-    
-    "DOD_fraction": 0.8,         # Fraction of battery capacity that can be used (Depth of Discharge)
-    "eta_battery": 0.95,         # Battery discharge efficiency
-    }
+    "DOD_fraction": 0.8,  # Fraction of battery capacity that can be used (Depth of Discharge)
+    "eta_battery": 0.95,  # Battery discharge efficiency
+}
 inputs.update(battery_parameters)
 
 V_range = np.arange(40, 70, 2)
 
 
-# Iteration loop 
+# Iteration loop
 # ==========================================
 
 tolerance = 1
 max_iterations = 200
-relevant = ['M_to', 'S_wing', 'b_wing', 'R_max', 'propeller_diameter', 'M_battery']
+relevant = ["M_to", "S_wing", "b_wing", "R_max", "propeller_diameter", "M_battery"]
 
 doesnt_converge = set()
-def integration_optimization(tolerance: float, max_iterations: int, inputs: dict[str, float | int]) -> tuple[dict[str, float | int], dict[str, list[float]]]:
-    '''
+
+
+def integration_optimization(
+    tolerance: float, max_iterations: int, inputs: dict[str, float | int]
+) -> tuple[dict[str, float | int], dict[str, list[float]]]:
+    """
     Combines the mass estimation, battery calculation and mission profile calculations and iterates until convergence or max iterations.
 
     Parameters:
-    tolerance (float): The tolerance for convergence.   
+    tolerance (float): The tolerance for convergence.
     max_iterations (int): The maximum number of iterations to perform.
     inputs (dict): A dictionary containing the input parameters for the calculations.
-    '''
+    """
     for i in range(max_iterations):
         uav_sizing = SizeUAV(inputs)
         outputs = uav_sizing.uav_profile().copy()
-        
 
         # Calculate the mass of the battery
         battery_mass = calculate_battery_mass(
-            E_required_Wh=inputs['total_mission_energy'],
-            DOD_fraction=inputs['DOD_fraction'],
-            eta_battery=inputs['eta_battery'],
-            M_to=inputs['M_to'] 
+            E_required_Wh=inputs["total_mission_energy"],
+            DOD_fraction=inputs["DOD_fraction"],
+            eta_battery=inputs["eta_battery"],
+            M_to=inputs["M_to"],
         )
-        
-       
-        outputs['M_battery'] = battery_mass
+
+        outputs["M_battery"] = battery_mass
 
         outputs = mass_sizing(outputs)
-        
 
-        if all(abs(outputs[key] - inputs[key]) < tolerance for key in outputs if isinstance(outputs[key], float) or isinstance(outputs[key], np.float64)):
-            print('Converged')
+        if all(
+            abs(outputs[key] - inputs[key]) < tolerance
+            for key in outputs
+            if isinstance(outputs[key], float) or isinstance(outputs[key], np.float64)
+        ):
+            print("Converged")
             return outputs
-        
-        if i > max_iterations-3:       
+
+        if i > max_iterations - 3:
             for key in outputs:
-                if isinstance(outputs[key], float) or isinstance(outputs[key],  np.float64):
+                if isinstance(outputs[key], float) or isinstance(
+                    outputs[key], np.float64
+                ):
                     if abs(outputs[key] - inputs[key]) > tolerance:
                         doesnt_converge.add(key)
 
         inputs = outputs
-    print('result did not stabilize at max iteration')
+    print("result did not stabilize at max iteration")
     return outputs
 
-# print(integration_optimization(1, 100, inputs))
+
+print(integration_optimization(1, 100, inputs))
+
 
 def speed_optimisation(inputs, V_range, max_to):
     all_outputs = []
     for V in V_range:
-        print('Calculating for V=', V)
-        inputs['V_cruise'] = V
-        inputs['V_stall'] = 0.5 * V
+        print("Calculating for V=", V)
+        inputs["V_cruise"] = V
+        inputs["V_stall"] = 0.5 * V
 
         outputs = integration_optimization(0.01, 100, inputs)
-        if outputs['M_to'] < max_to:
+        if outputs["M_to"] < max_to:
             all_outputs.append(outputs)
 
     if all_outputs:
         return all_outputs[-1]
     else:
-        return 'No viable outputs'
+        return "No viable outputs"
 
-print(speed_optimisation(inputs, V_range, 30))
+
+# print(speed_optimisation(inputs, V_range, 30))
