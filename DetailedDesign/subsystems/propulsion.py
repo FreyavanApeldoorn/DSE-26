fec382abbf0211579c5ef92a77c13bdd7b0ecbfb
Kaj <kajus.casias@gmail.com>
2025-05-27 09:53:42 +0200

    split power and propulsion

---
+++ DetailedDesign/subsystems/propulsion.py
@@ -0,0 +1,27 @@
+'''
+This is the file for the propulsion and power subsystem. It contains a single class.
+'''
+
+class Propulsion:
+
+    def __init__(self, inputs: dict[str, float]) -> None:
+        self.inputs = inputs
+        self.outputs = self.inputs.copy()
+
+    # ~~~ Intermediate Functions ~~~
+
+    def example_function(self):
+        '''
+        This is an example intermediate function
+        '''
+        return True
+
+    # ~~~ Output functions ~~~ 
+
+    def get_all(self) -> dict[str, float]:
+
+        return self.outputs
+    
+if __name__ == '__main__':
+    # Perform sanity checks here
+    ...
