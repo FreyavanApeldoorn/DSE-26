6c6746d42b59c2ca7a9e0424ede2d982cbfc2366
jpavilap <126147766+jpavilap@users.noreply.github.com>
2025-05-28 16:50:48 +0200

    a

--- DetailedDesign/funny_inputs.py
+++ DetailedDesign/funny_inputs.py
@@ -1,17 +1,18 @@
-'''
-This is the funny_inputs file, you are allowed and supposed to change this while coding. 
+"""
+This is the funny_inputs file, you are allowed and supposed to change this while coding.
 If your code is unfinished, please use these
-'''
+"""
+
+import numpy as np
 
 funny_inputs = {}
 
-# ~~~ Constants ~~~ 
+# ~~~ Constants ~~~
 
 constants_funny_inputs = {
-    'g': 9.81,           # Gravitational constant [m/s2]
-    'rho': 0.9093,       # Density at 3000m [kg/m^3]
-    'mtow': 30*9.81      # maximum takeoff weight [kg]
-
+    "g": 9.81,  # Gravitational constant [m/s2]
+    "rho": 0.9093,  # Density at 3000m [kg/m^3]
+    "mtow": 30 * 9.81,  # maximum takeoff weight [kg]
 }
 
 funny_inputs.update(constants_funny_inputs)
@@ -25,39 +26,32 @@ funny_inputs.update(aerodynamics_funny_inputs)
 # ~~~ Deployment ~~~~
 
 deployment_funny_inputs = {
-    'payload_mass' : 5., # kg
-    'aerogel_width' : 1.5, # m
-    'aerogel_thickness' : 0.003, # m
-    'aerogel_density' : 200., # kg/m3
-
-    'n_ferro_magnets' : 2, # nr
-    'ferro_magnet_mass' : 0.5, # kg, guesstimate
-    'deployment_added_mass' : 1., # kg, guesstimate
-
-    'wire_length' : 15., # m, neglecting the split ends of the wire
-    'wire_density' : 0.0149, # kg/m
-    'n_wire' : 2, # nr
-
-    'spring_mass' : 0., # kg
-    'winch_mass' : 1.5, #kg
-    'n_pulleys' : 4, # nr
-    'pulley_mass' : 0.2, # kg, guesstimate
-    'n_epms' : 4, # nr
-    'epm_diameter'  : 0.0025, # m
-    'epm_mass' : 0.039, # kg
-
-    'deployment_system_volume' : 0.0026554, # m3, guesstimate only incl. winch volume
-    'deployment_speed' : 0.3, # m/s
-    'deployment_time_margin' : 30., # s, guesstimate for aerogel unrolling, final positioning, and dropping
-
-    'power_required_epm' : 20., # W
-    'epm_duration' : 8., # s, maximum OFF-mode duration
-    'power_required_winch' : 96., # W
-
-    'deployment_accuracy' : 0.5, # m, guesstimate
-    'firebreak_width': 3, # m
-
-    'fuselage_size' : 1.5, # m, guesstimate
+    "payload_mass": 5.0,  # kg
+    "aerogel_width": 1.5,  # m
+    "aerogel_thickness": 0.003,  # m
+    "aerogel_density": 200.0,  # kg/m3
+    "n_ferro_magnets": 2,  # nr
+    "ferro_magnet_mass": 0.5,  # kg, guesstimate
+    "deployment_added_mass": 1.0,  # kg, guesstimate
+    "wire_length": 15.0,  # m, neglecting the split ends of the wire
+    "wire_density": 0.0149,  # kg/m
+    "n_wire": 2,  # nr
+    "spring_mass": 0.0,  # kg
+    "winch_mass": 1.5,  # kg
+    "n_pulleys": 4,  # nr
+    "pulley_mass": 0.2,  # kg, guesstimate
+    "n_epms": 4,  # nr
+    "epm_diameter": 0.0025,  # m
+    "epm_mass": 0.039,  # kg
+    "deployment_system_volume": 0.0026554,  # m3, guesstimate only incl. winch volume
+    "deployment_speed": 0.3,  # m/s
+    "deployment_time_margin": 30.0,  # s, guesstimate for aerogel unrolling, final positioning, and dropping
+    "power_required_epm": 20.0,  # W
+    "epm_duration": 8.0,  # s, maximum OFF-mode duration
+    "power_required_winch": 96.0,  # W
+    "deployment_accuracy": 0.5,  # m, guesstimate
+    "firebreak_width": 3,  # m
+    "fuselage_size": 1.5,  # m, guesstimate
 }
 
 funny_inputs.update(deployment_funny_inputs)
@@ -68,24 +62,36 @@ operations_funny_inputs = {}
 
 funny_inputs.update(operations_funny_inputs)
 
-# ~~~ Propulsion and Power ~~~ 
+# ~~~ Propulsion and Power ~~~
 
 prop_n_pow_funny_inputs = {
-    'wing_loading' : 5., # kg
-    'aerogel_thickness' : 0.003, # m
-    'aerogel_density' : 200., # kg/m3
-    'vtol_roc' : 1.5, # m/s
+    "wing_loading": 5.0,  # kg
+    "aerogel_thickness": 0.003,  # m
+    "aerogel_density": 200.0,  # kg/m3
+    "vtol_roc": 1.5,  # m/s
 }
 
 funny_inputs.update(prop_n_pow_funny_inputs)
 
-# ~~~ Stability and control ~~~ 
+# ~~~ Stability and control ~~~
 
-stab_n_con_funny_inputs = {}
+stab_n_con_funny_inputs = {
+    "ca_c": 0.4,  # Aileron chord to wing chord ratio
+    "cl_alpha": 5.0 * 180 / np.pi,  # Wing airfoil (E1210) lift curve slope [1/rad]
+    "cd_0": 0.02,  # Wing airfoil (E1210)Zero-lift drag coefficient
+    "wing_area": 1.0,  # m^2, guesstimate
+    "wing_span": 3.0,  # m, guesstimate
+    "wing_chord": 0.333,  # m, guesstimate
+    "bi": 0.1,  # m, location to the innermost point of the aileron
+    "bo": 0.9,  # m, location to the outermost point of the aileron
+    "delta_a_max": np.deg2rad(25),  # rad, maximum aileron deflection angle
+    "aileron_differential": 0.75,  # Aileron differential, ratio of down-going to up-going aileron deflection
+    "roll_rate_req": 0.5,  # rad/s, guesstimate
+}
 
 funny_inputs.update(stab_n_con_funny_inputs)
 
-# ~~~ Structures ~~~ 
+# ~~~ Structures ~~~
 
 structures_funny_inputs = {}
 
--- DetailedDesign/subsystems/stab_n_con.py
+++ DetailedDesign/subsystems/stab_n_con.py
@@ -1,33 +1,113 @@
-'''
+"""
 This is the file for stability and control subsystem. It contains a single class.
-'''
+"""
+
+import numpy as np
+from scipy.integrate import simpson
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from DetailedDesign.funny_inputs import stab_n_con_funny_inputs as fi
+
 
 class StabCon:
 
     def __init__(self, inputs: dict[str, float]) -> None:
         self.inputs = inputs
+        self.ca_c = self.inputs["ca_c"]  # Aileron chord to wing chord ratio
+        self.delta_a_max = self.inputs[
+            "delta_a_max"
+        ]  # Maximum deflection angle of the ailerons
+        self.aileron_differential = self.inputs["aileron_differential"]
+        self.wing_area = self.inputs["wing_area"]  # Wing area
+        self.wing_span = self.inputs["wing_span"]  # Wing span
+        self.wing_chord = self.inputs["wing_chord"]  # Wing chord
+        self.bi = self.inputs["bi"]  # Location to the innermost point of the aileron
+        self.bo = self.inputs["bo"]
+        self.roll_rate_req = self.inputs["roll_rate_req"]  # Roll rate requirement
+        self.cl_alpha = self.inputs["cl_alpha"]
+        self.cd_0 = self.inputs["cd_0"]
+
         self.outputs = self.inputs.copy()
 
     # ~~~ Intermediate Functions ~~~
 
-    def example_function(self):
-        '''
-        This is an example intermediate function
-        '''
-        return True
+    def tau_from_ca_over_c(self):
+        """
+        Calculate elevator effectiveness (τ) from the ratio of control-surface chord to lifting-surface chord (cₐ/c).
 
-    # ~~~ Output functions ~~~ 
+        Returns
+        -------
+        float
+            The effectiveness parameter τ corresponding to self.iinputs["ca_c"], by linear interpolation of the data in data/elevator_effectiveness.csv.
+        """
+        # Load two columns (ca/c , τ) from the CSV, skipping the header
+        data = np.loadtxt(
+            "DetailedDesign\data\elevator_effectiveness.csv", delimiter=",", skiprows=0
+        )
+
+        # Split into x-values (ca/c) and y-values (τ)
+        x = data[:, 0]
+        tau = data[:, 1]
+
+        # Interpolate τ at the desired ratio
+        return np.interp(self.ca_c, x, tau)
+
+    def size_ailerons(self):
+        """
+        Calculate the size of the ailerons, based on:
+        - The elevator effectiveness (τ) calculated from the ratio of control-surface chord to lifting-surface chord (cₐ/c).
+        - The maximum deflection angle of the ailerons (delta_a_max).
+        - The aileron differential (aileron_differential).
+        - The wing area (wing_area).
+        - The wing span (wing_span).
+        - The wing chord (wing_chord).
+        - The location to the innermost point of the aileron (bi).
+        - The location to the outermost point of the aileron (bo).
+        - The roll rate requirement (roll_rate_req).
+        - The lift curve slope of the wing airfoil (cl_alpha).
+        - The zero-lift drag coefficient of the wing airfoil (cd_0).
+
+
+        Returns
+        -------
+
+        """
+        ### TEMPORARY CREATE WING CHORD ARRAY AS A FUNCTION OF WING SPAN ASSUMING NON-TAPERED WING###
+        y = np.linspace(0, self.wing_span / 2, 100)  # Half span
+        c = self.inputs["wing_chord"] * y
+
+        # Mask for the aileron span
+        mask = (y >= self.bi) & (y <= self.bo)
+        ### END TEMPORARY WING CHORD ARRAY CREATION ###
+
+        # Calculate control derivative Cl_a
+        Cl_a = (
+            (2 * self.cl_alpha * self.tau_from_ca_over_c())
+            / (self.wing_area * self.wing_span)
+        ) * simpson(c[mask] * y[mask], y[mask])
+
+        # Calculate stability derivative Cl_p
+        Cl_p = -(
+            (4 * (self.cl_alpha * self.cd_0)) / (self.wing_area * self.wing_span)
+        ) * simpson(c * y**2, y[mask])
+
+        return
+
+    # ~~~ Output functions ~~~
 
     def get_all(self) -> dict[str, float]:
-
+        """
         # These are all the required outputs for this class. Plz consult the rest if removing any of them!
 
-        outputs["Power_required"] = ... # New requirement for power in order to get the right control authority
+        outputs["Power_required"] = (
+            ...
+        )  # New requirement for power in order to get the right control authority
 
         outputs["Propeller_arm_length"] = ...
         outputs["Tail_arm"] = ...
         outputs["Tail_area"] = ...
-
 
         outputs["CG_x_max"] = ...
         outputs["CG_y_max"] = ...
@@ -35,10 +115,12 @@ class StabCon:
         outputs["CG_y_min"] = ...
 
         # something about control surfaces
+        """
+        return "self.outputs"
 
 
-        return self.outputs
-    
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Perform sanity checks here
-    ...
+    A = StabCon(fi)
+
+    print(A.tau_from_ca_over_c())
