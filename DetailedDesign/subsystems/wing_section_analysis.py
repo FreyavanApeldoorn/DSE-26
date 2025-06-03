aee4415880464550bf066b326501da8a1d78973d
Wietske Overdijkink <w.m.overdijkink@student.tudelft.nl>
2025-06-03 12:07:00 +0200

    airfoil

--- DetailedDesign/subsystems/wing_section_analysis.py
+++ DetailedDesign/subsystems/wing_section_analysis.py
@@ -1,16 +1,16 @@
 #wing section analysis
 
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 
 # Read the CSV file (update the path if needed)
-csv_path = r"C:\Users\wiets\OneDrive\Documenten\TU Delft\BEP\DSE-26\DetailedDesign\data\elevator_effectiveness.csv"
-data = pd.read_csv(csv_path, header=None, names=['x', 'y'])
+csv_path = r"C:\Users\wiets\OneDrive\Documenten\TU Delft\BEP\DSE-26\DetailedDesign\data\e1212_Lednicer.DAT"
+data = pd.read_csv(csv_path, skiprows=2, header=None, names=['x', 'y'], delim_whitespace=True)
 
-# Assuming the CSV has columns 'x' and 'y'
 plt.figure(figsize=(10, 4))
 plt.plot(data['x'], data['y'], marker='o')
-plt.title('Airfoil Shape from elevator_effectiveness.csv')
+plt.title('Airfoil Shape from e1212 Lednicer Data')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.axis('equal')
