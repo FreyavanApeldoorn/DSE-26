02e3bccdfadad4ab12e0cb88d458a3b9a87712eb
lsijgers <l.sijgers@student.tudelft.nl>
2025-06-04 11:35:13 +0200

    Changed some namings

--- DetailedDesign/subsystems/aerodynamics.py
+++ DetailedDesign/subsystems/aerodynamics.py
@@ -1,6 +1,13 @@
 '''
 This is the file for the aerodynamics subsystem. It contains a single class.
 '''
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+
+# Add the project root to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 class Aerodynamics:
 
@@ -9,8 +16,8 @@ class Aerodynamics:
         self.outputs = self.inputs.copy()
 
         #Wing Parameters
-        self.aspect_ratio = inputs["aspect_ratio"]  # Wing aspect ratio 
-        self.wing_area = inputs["wing_area"]  # Wing area in m^2 
+        self.maximum_takeoff_weight = inputs["MTOW"]
+        self.aspect_ratio = inputs["AR"]  # Wing aspect ratio 
         self.taper_ratio = inputs["taper_ratio"]  # Wing taper ratio
         self.sweep_angle = inputs["sweep_angle"] # Sweep angle of the wing in degrees
         self.thickness_to_chord_ratio = inputs["thickness_to_chord_ratio"]  # Thickness to chord ratio for the wing max
@@ -30,11 +37,22 @@ class Aerodynamics:
         self.sweep_angle_vertical_tail = inputs["sweep_angle_vertical_tail"]  # Sweep angle of the vertical tail in degrees
         self.relative_vertical_tail_aspect_ratio = inputs["relative_vertical_tail_aspect_ratio"]  # Relative aspect ratio of the vertical tail to the wing
 
+        #Gust inputs
+
+        # Maneuver inputs
+        self.max_load_factor = inputs["max_load_factor"]  # Maximum load factor for maneuvering
+        self.min_load_factor = inputs["min_load_factor"]  # Minimum load factor for maneuvering
+        self.density_sea = inputs["density_sea"]  # Air density in kg/m^3
+        self.density_3000 = inputs["density_3000"]  # Air density at 3000m in kg/m^3
+        self.CL_max = inputs["CL_max"]  # Maximum lift coefficient
+        self.wing_loading = inputs["wing_loading"]  # Wing loading in N/m^2
+        self.cruise_velocity = inputs["cruise_velocity"]  # Cruise velocity in m/s
 
 
     # ~~~ Intermediate Functions ~~~
 
     def calculate_wing_parameters(self) -> dict[str, float]:
+        self.wing_area = self.maximum_takeoff_weight / self.wing_loading  
         self.wing_span = (self.wing_area * self.aspect_ratio) ** 0.5  # Wing span in m
         self.wing_chord = self.wing_area / self.wing_span
         self.wing_root_chord = (2 * self.wing_chord) / (1 + self.taper_ratio) # Root chord length
@@ -61,18 +79,37 @@ class Aerodynamics:
         self.vertical_tail_tip_chord = self.taper_ratio_vertical_tail * self.vertical_tail_root_chord  # Tip chord length for vertical tail m
         self.vertical_tail_MAC = (2 / 3) * self.vertical_tail_root_chord * ((1 + self.taper_ratio_vertical_tail + self.taper_ratio_vertical_tail**2) / (1 + self.taper_ratio_vertical_tail))  # Mean Aerodynamic Chord for vertical tail m
 
+    def gust_diagram(self) -> dict[str, float]:
+
+        raise NotImplementedError("Gust diagram calculation is not implemented yet.")
+    
+    def maneuver_diagram(self, dive_factor) -> dict[str, float]:
+        maneuver_velocity_0A = np.arange(0, self.cruise_velocity, 0.1)
+        total_velocity_range = np.arange(0, self.cruise_velocity * dive_factor, 0.1)
+
+        n_0A_sea = (0.5 * self.density_sea * maneuver_velocity_0A**2 * self.CL_max)/ self.wing_loading  # Load factor for maneuvering
+        n_0A_3000 = (0.5 * self.density_3000 * maneuver_velocity_0A**2 * self.CL_max)/ self.wing_loading  # Load factor for maneuvering at 3000m
+
+        n_0H_sea = -1 * (0.5 * self.density_sea * maneuver_velocity_0A**2 * self.CL_max)/ self.wing_loading
+        n_0H_3000 = -1 * (0.5 * self.density_3000 * maneuver_velocity_0A**2 * self.CL_max)/ self.wing_loading
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(maneuver_velocity_0A, n_0A_sea, label='Load Factor vs Velocity')
+        plt.plot(maneuver_velocity_0A, n_0A_3000, label='Load Factor vs Velocity at 3000m')
+
+        plt.plot(total_velocity_range, )
+
+        plt.plot(maneuver_velocity_0A, n_0H_sea, label='Negative Load Factor vs Velocity')
+        plt.plot(maneuver_velocity_0A, n_0H_3000, label='Negative Load Factor vs Velocity at 3000m')
+
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
     # ~~~ Output functions ~~~ 
 
     def get_all(self) -> dict[str, float]:
 
-        # These are all the required outputs for this class. Plz consult the rest if removing any of them!
-
-        #outputs["CL_cruise"] = ...
-        #outputs["CD_cruise"] = ...
-        #outputs["CL_max"] = ...
-        #outputs["CD_max"] = ...
-        
         self.calculate_wing_parameters()
         self.calculate_horizontal_tail_parameters()
         self.calculate_vertical_tail_parameters()
@@ -88,7 +125,7 @@ class Aerodynamics:
         self.outputs["Wing_MAC"] = self.MAC
 
         self.outputs["horizontal_tail_length"] = self.horizontal_tail_length
-        self.outptuts["horizontal_tail_area"] = self.horizontal_tail_area
+        self.outputs["horizontal_tail_area"] = self.horizontal_tail_area
         self.outputs["horizontal_tail_span"] = self.horizontal_tail_span
         self.outputs["horizontal_tail_chord"] = self.horizontal_tail_chord
         self.outputs["horizontal_tail_root_chord"] = self.horizontal_tail_root_chord
@@ -116,5 +153,8 @@ class Aerodynamics:
         return self.outputs
     
 if __name__ == '__main__':
+    from DetailedDesign.funny_inputs import aerodynamics_funny_inputs
+    aerodynamics = Aerodynamics(aerodynamics_funny_inputs)
+    aerodynamics.maneuver_diagram(1.4) # Example dive factor
     # Perform sanity checks here
     ...
